7441cf792ffa44a37676eadab53c392cab4234fc
eginoy <eginoy.dev@gmail.com>
2024-08-11 09:53:42 +0000

    chapter3: refactor

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,33 +1,42 @@
-use clap::{value_parser, Arg, ArgAction, Command};
-use std::error::Error;
+use anyhow::Result;
+use clap::Parser;
 use std::fmt::Debug;
 use std::fs::File;
-use std::io::{self, BufRead, BufReader, Read};
+use std::io::{self, BufRead, BufReader};
 
-#[derive(Debug)]
-pub struct Config {
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+pub struct Arg {
+    #[arg(value_name = "FILE", default_value = "-")]
     files: Vec<String>,
+
+    #[arg(short('n'), long("number"), conflicts_with("number_nonblank"))]
     number_lines: bool,
+
+    #[arg(short('b'), long("number-nonblank"))]
     number_nonblank: bool,
 }
 
-type MyResult<T> = Result<T, Box<dyn Error>>;
-
-pub fn run(config: Config) -> MyResult<()> {
+pub fn run(config: Arg) -> Result<()> {
     for filename in config.files {
         match open(&filename) {
             Err(err) => eprintln!("Failed to open {}: {}", filename, err),
-            Ok(buf) => {
-                let mut r = BufReader::new(buf);
-                let mut text = String::from("");
-                _ = r.read_to_string(&mut text);
-
-                if config.number_lines {
-                    print!("{}", format_number_lines(&text))
-                } else if config.number_nonblank {
-                    print!("{}", format_number_nonblank(&text))
-                } else {
-                    print!("{}", &text)
+            Ok(file) => {
+                let mut line_number_nonblank = 1;
+                for (line_num, line) in file.lines().enumerate() {
+                    let line = line?; //シャドーイング
+                    if config.number_lines {
+                        println!("{:6}\t{line}", line_num + 1);
+                    } else if config.number_nonblank {
+                        if line.is_empty() {
+                            println!()
+                        } else {
+                            println!("{line_number_nonblank:6}\t{line}",);
+                            line_number_nonblank += 1;
+                        }
+                    } else {
+                        println!("{}", line);
+                    }
                 }
             }
         }
@@ -35,76 +44,9 @@ pub fn run(config: Config) -> MyResult<()> {
     Ok(())
 }
 
-pub fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+pub fn open(filename: &str) -> Result<Box<dyn BufRead>> {
     match filename {
         "-" => Ok(Box::new(BufReader::new(io::stdin()))),
         _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
     }
-}
-
-pub fn format_number_lines(text: &str) -> String {
-    let mut result = String::from("");
-    let mut i = 1;
-    for line in text.to_string().lines() {
-        result += format!("     {}\t{}\n", i, line).as_str();
-        i += 1;
-    }
-
-    result
-}
-
-pub fn format_number_nonblank(text: &str) -> String {
-    let mut result = String::from("");
-    let mut i = 1;
-    for line in text.to_string().lines() {
-        if line.is_empty() {
-            result += "\n";
-        } else {
-            result += format!("     {}\t{}\n", i, line).as_str();
-            i += 1;
-        };
-    }
-
-    result
-}
-
-pub fn get_args() -> MyResult<Config> {
-    let m = Command::new("catr")
-        .version("0.1.0")
-        .author("eginoy")
-        .about("Rust cat")
-        .arg(
-            Arg::new("files")
-                .action(ArgAction::Append)
-                .value_parser(value_parser!(String))
-                .default_value("-")
-                .help("concatenate files"),
-        )
-        .arg(
-            Arg::new("number_lines")
-                .value_name("NUMBER_LINE")
-                .long("number")
-                .short('n')
-                .help("number all output lines")
-                .action(ArgAction::SetTrue)
-                .conflicts_with("number_nonblank"),
-        )
-        .arg(
-            Arg::new("number_nonblank")
-                .value_name("NUMBER_NONBLANK")
-                .long("number-nonblank")
-                .short('b')
-                .help("number nonempty output lines")
-                .action(ArgAction::SetTrue)
-                .conflicts_with("number_lines"),
-        )
-        .get_matches();
-
-    let files: Vec<String> = m.get_many("files").unwrap().cloned().collect();
-
-    Ok(Config {
-        files: files,
-        number_lines: m.get_flag("number_lines"),
-        number_nonblank: m.get_flag("number_nonblank"),
-    })
 }
--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,5 +1,7 @@
+use clap::Parser;
+
 fn main() {
-    if let Err(e) = catr::get_args().and_then(catr::run) {
+    if let Err(e) = catr::run(catr::Arg::parse()) {
         eprintln!("{}", e);
         std::process::exit(1);
     }
